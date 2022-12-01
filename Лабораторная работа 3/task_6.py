4ce969dbd6728db081dbd02d989a5944cfed0821
i-SAVE <119124693+i-SAVE@users.noreply.github.com>
2022-12-01 17:18:09 +0300

    Update task_6.py
    Исправил task_6 по полученному отзыву

--- Лабораторная работа 3/task_6.py
+++ Лабораторная работа 3/task_6.py
@@ -1,14 +1,22 @@
 list_numbers = [2, 90, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-max_res = 0
-last_value = list_numbers[-1]
+
 # TODO Оформить решение
-for i in range(len(list_numbers)):  # перебираем все индксы
-    max_value = list_numbers[max_res]  # присвоим переменной значение потенциального макс значения
-    if list_numbers[i] >= max_value:  # если текущее значение больше max, переобозначим индекс максимального значения
-        max_res = i
 
-# поменяем местами последний элемент и последний макс элемент
-list_numbers[-1] = list_numbers[max_res]
-list_numbers[max_res] = last_value
+max_index = 0  # предпологаем, что индекс max значения это "0"
+last_index = -1  # обозначаем индекс послденего значения списка
 
+max_number = list_numbers[max_index]  # предпологаю, что max значение это значение под max_index
+last_value = list_numbers[last_index]  # выписываю посл. значение списка
+
+for i, current_number in enumerate(list_numbers):
+    # выписваю цикл с командой enumerate
+    if current_number >= max_number:  # пишу условие - если текущее значение списка > или = max значения
+        max_number = current_number  # (предпологаемого) - то я меняю его на текцщее в цикле (и значение и индекс)
+        max_index = i
+# получив максимальное знаяение перебрав весь список благодаря циклу и условию перейдем к смене мест значений
+
+list_numbers[max_index], list_numbers[last_index] = list_numbers[last_index], list_numbers[max_index]
+# используб множеств. присваивание, чтобы поменять местами max_number (макс.число и посл.число)
 print(list_numbers)
+
+# Ответ [2, 90, -2, 8, -36, -44, -1, -85, -14, 25, -22, -90, -100, -8, 38, -92, -45, 67, 53, 90]
